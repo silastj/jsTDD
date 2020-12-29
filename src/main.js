91551d3c6a22822b505e2a282735d9bc80a1e5c6
aSilas <silastj@hotmail.com>
2020-12-29 20:13:28 -0300

    add Husky

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const amor = 3;
 const bola = 2;
-const leftAMor = 4
+// const leftAMor = 4;
 const novo = 'novo'
 const casa = 'dasdasdsdadsdasdsdsdadsdasdsdadasasdsaddadadsassdasddsaasdad';
 console.log(novo,casa,amor,bola,leftAMor)
