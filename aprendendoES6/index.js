549208f191062e4736874556ca6d557d0be9efb7
aSilas <silastj@hotmail.com>
2021-01-06 23:44:00 -0300

    declaração de variaveis

---
+++ aprendendoES6/index.js
@@ -0,0 +1,28 @@
+var animal = 'dog'
+console.log(animal)
+  {
+    var animal = 'cat'
+    console.log(animal)
+  }
+console.log(animal)
+
+// LET
+let Carros = 'Punto'
+console.log(Carros)
+  {
+    let Carros = "Celta"
+    console.log(Carros)
+  }
+
+// CONST
+const celular = {
+  marca: 'Apple',
+  modelo: 'iphone 7'
+}
+console.log(celular.marca)
+  celular.marca = 'Motorola'
+console.log(celular.marca)
+
+
+
+
