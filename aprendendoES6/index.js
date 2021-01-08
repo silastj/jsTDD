2196a4acca4e5b12f4e7dd0b042158d10eaf5192
aSilas <silastj@hotmail.com>
2021-01-07 23:36:18 -0300

    Arrow Function

--- aprendendoES6/index.js
+++ aprendendoES6/index.js
@@ -23,6 +23,28 @@ console.log(celular.marca)
   celular.marca = 'Motorola'
 console.log(celular.marca)
 
+// ARROW FUNCTION
+const cidade = ['Ferraz','Poá','Suzano']
+const love = cidade.map(function (name) {
+  return `I love ${name}`
+})
+const loveArrow = cidade.map((name) => {
+  return `I Love ${name}`
+})
+const loveSingle = cidade.map(name => {
+  return `I Love ${name}`
+})
+const loveOneLinha = cidade.map(name => `I Love ${name}`)
+const loveChain = cidade
+                        .filter(name => name === 'Poá')
+                        .map(name => `I Love ${name}`)
+console.log(love, loveArrow, loveSingle, loveOneLinha, loveChain)
 
-
-
+//ARROW FUNCTION THIS
+const buttonLove = document.querySelector('button')
+buttonLove.addEventListener('click', () => {
+  console.log(this)
+})
+buttonLove.addEventListener('click', function() {
+  console.log(this)
+})
